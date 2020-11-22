38fad4bf6c9113dbbcd28dce7b1490c27daa4bec
Robert <robertjwayne@gmail.com>
2020-11-22 04:25:07 -0800

    Add game functionality
    - Add functionality to tick through generations
    - Add functionality to reset to initial generation
    - Add functionality for user to click on a cell and toggle whether it is dead or alive
    - Add functionality to resize the board grid
    - Add functionality to change the ticking speed
    - Add functionality for selecting preset patterns

---
+++ src/app/game-board/game-board.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import * as GameBoardActions from './store/game-board.actions';
+import * as fromGameBoard from './store/game-board.reducer';
+
+
+@Component({
+  selector: 'app-game-board',
+  templateUrl: './game-board.component.html',
+  styleUrls: ['./game-board.component.css']
+})
+export class GameBoardComponent implements OnInit, OnDestroy {
+  currentGeneration: number[][];
+  subscription: Subscription;
+  ticker = null;
+  currentlyTicking = false;
+  tickInterval = 500;
+
+  constructor(private store: Store<{ gameBoard: fromGameBoard.State }>) { }
+
+  ngOnInit(): void {
+    this.subscription = this.store.select('gameBoard').subscribe(state => {
+      this.currentGeneration = state.currentGeneration;
+    });
+  }
+
+  tick() {
+    this.store.dispatch(GameBoardActions.tick());
+  }
+
+  reset() {
+    this.store.dispatch(GameBoardActions.reset());
+  }
+
+  handleCellClick(rowIndex, columnIndex) {
+    this.store.dispatch(GameBoardActions.toggleCellLife({ rowIndex, columnIndex }));
+  }
+
+  handleGridResize(event) {
+    this.setGridSize(event.target.value);
+  }
+
+  setGridSize(gridSize) {
+    this.store.dispatch(GameBoardActions.setGridSize({ gridSize }));
+  }
+
+  startTicking() {
+    if (this.currentlyTicking) this.stopTicking();
+    this.ticker = setInterval(() => {
+      this.store.dispatch(GameBoardActions.tick());
+    }, this.tickInterval);
+    this.currentlyTicking = true;
+  }
+
+  stopTicking() {
+    clearInterval(this.ticker);
+    this.ticker = null;
+    this.currentlyTicking = false;
+  }
+
+  handleSpeedChange(event) {
+    const rangeValue = event.target.value;
+    const rangeMax = event.target.getAttribute('max');
+    const newTickInterval = rangeMax - rangeValue;
+    this.tickInterval = newTickInterval;
+    if (this.currentlyTicking) this.startTicking();
+  }
+
+  handlePatternSelect(event) {
+    const patternName = event.target.value;
+    this.store.dispatch(GameBoardActions.selectPattern({ patternName }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
