1f8222b01dbdbade710b6b5f8209bafd0945c207
Robert <robertjwayne@gmail.com>
2020-11-22 04:29:29 -0800

    Add GameBoardComponent, StoreModule, StoreDevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+import * as fromGameBoard from './game-board/store/game-board.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ gameBoard: fromGameBoard.gameBoardReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
