c1227b3e411d21de6289462c449ced1275612917
Robert <robertjwayne@gmail.com>
2020-11-26 13:14:29 +0700

    Rearrange imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
-import * as fromGameBoard from './game-board/store/game-board.reducer';
 import { ConfigSliderComponent } from './game-board/game-config/config-slider/config-slider.component';
 import { PatternsComponent } from './patterns/patterns.component';
 import { GameConfigComponent } from './game-board/game-config/game-config.component';
 import { GameCounterComponent } from './game-board/game-counter/game-counter.component';
 import { ConfigCheckboxComponent } from './game-board/game-config/config-checkbox/config-checkbox.component';
-import { EffectsModule } from '@ngrx/effects';
 import { GameBoardEffects } from './game-board/store/game-board.effects';
+import * as fromGameBoard from './game-board/store/game-board.reducer';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,7 @@ import { GameBoardEffects } from './game-board/store/game-board.effects';
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
     }),
-    EffectsModule.forRoot([GameBoardEffects])
+    EffectsModule.forRoot([GameBoardEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent]
