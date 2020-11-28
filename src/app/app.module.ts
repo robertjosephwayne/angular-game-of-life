8df134ab97241f9e6a30c56614c18f0cbf37b9b8
Robert <robertjwayne@gmail.com>
2020-11-28 13:23:35 +0700

    Install material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
-import { ConfigSliderComponent } from './game-board/game-config/config-slider/config-slider.component';
+import { ConfigSliderComponent } from './game-config/config-slider/config-slider.component';
 import { PatternsComponent } from './patterns/patterns.component';
-import { GameConfigComponent } from './game-board/game-config/game-config.component';
-import { GameCounterComponent } from './game-board/game-counter/game-counter.component';
-import { ConfigCheckboxComponent } from './game-board/game-config/config-checkbox/config-checkbox.component';
-import { GameBoardEffects } from './game-board/store/game-board.effects';
+import { GameConfigComponent } from './game-config/game-config.component';
+import { GameCounterComponent } from './game-counter/game-counter.component';
+import { ConfigCheckboxComponent } from './game-config/config-checkbox/config-checkbox.component';
 import * as fromGameBoard from './game-board/store/game-board.reducer';
+import { GameHeaderComponent } from './game-header/game-header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import * as fromGameBoard from './game-board/store/game-board.reducer';
     PatternsComponent,
     GameConfigComponent,
     GameCounterComponent,
-    ConfigCheckboxComponent
+    ConfigCheckboxComponent,
+    GameHeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +33,8 @@ import * as fromGameBoard from './game-board/store/game-board.reducer';
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
     }),
-    EffectsModule.forRoot([GameBoardEffects]),
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
