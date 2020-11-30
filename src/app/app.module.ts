4e890b715cafe6b20e73610e76720e737f2115b6
Robert <robertjwayne@gmail.com>
2020-11-30 19:12:15 +0700

    Rearrange imports for readability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { MaterialModule } from './material.module';
+
+import * as fromGameBoard from './game-board/store/game-board.reducer';
 
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { PatternsComponent } from './patterns/patterns.component';
 import { GameConfigComponent } from './game-config/game-config.component';
 import { GameCounterComponent } from './game-counter/game-counter.component';
-import * as fromGameBoard from './game-board/store/game-board.reducer';
 import { GameHeaderComponent } from './game-header/game-header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { EffectsModule } from '@ngrx/effects';
 import { GameBoardEffects } from './game-board/store/game-board.effects';
 import { GameButtonsComponent } from './game-buttons/game-buttons.component';
 import { GameGridComponent } from './game-grid/game-grid.component';
