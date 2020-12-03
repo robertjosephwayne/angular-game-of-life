56dbbd42cd67610b4856e11c120e135d3b7ba383
Robert <robertjwayne@gmail.com>
2020-12-03 14:53:53 +0700

    Add three new actions
    Add the following actions:
    - incrementGenerationCount
    - resetGenerationCount
    - setLiveCellCount

---
+++ src/app/game-config/game-stats/store/game-stats.reducer.ts
@@ -0,0 +1,42 @@
+import { Action, createReducer, on } from '@ngrx/store';
+
+import * as GameStatsActions from './game-stats.actions';
+
+export interface State {
+  generationCount: number;
+  liveCells: number;
+}
+
+export const initialState: State = {
+  generationCount: 0,
+  liveCells: 0
+};
+
+const _gameStatsReducer = createReducer(
+  initialState,
+
+  on(GameStatsActions.incrementGenerationCount, (state) => {
+    return {
+      ...state,
+      generationCount: state.generationCount + 1
+    };
+  }),
+
+  on(GameStatsActions.resetGenerationCount, (state) => {
+    return {
+      ...state,
+      generationCount: 0
+    };
+  }),
+
+  on(GameStatsActions.setLiveCellCount, (state, { liveCells }) => {
+    return {
+      ...state,
+      liveCells
+    };
+  })
+);
+
+export function gameStatsReducer(state: State, action: Action) {
+  return _gameStatsReducer(state, action);
+}
