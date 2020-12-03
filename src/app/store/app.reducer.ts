8fee06c7a782f5c596010e3ee6352b7a0e14b8db
Robert <robertjwayne@gmail.com>
2020-12-03 14:58:17 +0700

    Add GameConfig, GameStats, and Patterns to state.

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,11 +1,20 @@
 import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromGameBoard from '../game-board/store/game-board.reducer';
+import * as fromGameConfig from '../game-config/store/game-config.reducer';
+import * as fromGameStats from '../game-config/game-stats/store/game-stats.reducer';
+import * as fromPatterns from '../game-config/patterns/store/patterns.reducer';
 
 export interface AppState {
   gameBoard: fromGameBoard.State;
+  gameConfig: fromGameConfig.State;
+  gameStats: fromGameStats.State;
+  patterns: fromPatterns.State;
 }
 
 export const appReducer: ActionReducerMap<AppState> = {
-  gameBoard: fromGameBoard.gameBoardReducer
+  gameBoard: fromGameBoard.gameBoardReducer,
+  gameConfig: fromGameConfig.gameConfigReducer,
+  gameStats: fromGameStats.gameStatsReducer,
+  patterns: fromPatterns.patternsReducer
 };
