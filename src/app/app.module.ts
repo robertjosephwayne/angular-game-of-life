0d8d465979d8e37799ac00c6255453bbdcf9dd03
Robert <robertjwayne@gmail.com>
2020-12-03 14:49:18 +0700

    Add new stores
    Add gameConfig, gameStats, and patterns reducers to state.
    
    Add GameConfig, GameStats, and Patterns effects.
    
    Remove GameGrid component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,37 +7,54 @@ import { EffectsModule } from '@ngrx/effects';
 import { MaterialModule } from './material.module';
 
 import * as fromGameBoard from './game-board/store/game-board.reducer';
+import * as fromGameConfig from './game-config/store/game-config.reducer';
+import * as fromGameStats from './game-config/game-stats/store/game-stats.reducer';
+import * as fromPatterns from './game-config/patterns/store/patterns.reducer';
+
+import { GameBoardEffects } from './game-board/store/game-board.effects';
+import { GameConfigEffects } from './game-config/store/game-config.effects';
+import { GameStatsEffects } from './game-config/game-stats/store/game-stats.effects';
+import { PatternsEffects } from './game-config/patterns/store/patterns.effects';
 
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
-import { PatternsComponent } from './patterns/patterns.component';
-import { GameConfigComponent } from './game-config/game-config.component';
-import { GameCounterComponent } from './game-counter/game-counter.component';
-import { GameHeaderComponent } from './game-header/game-header.component';
-import { GameBoardEffects } from './game-board/store/game-board.effects';
 import { GameButtonsComponent } from './game-buttons/game-buttons.component';
-import { GameGridComponent } from './game-grid/game-grid.component';
+import { GameConfigComponent } from './game-config/game-config.component';
+import { GameCounterComponent } from './game-config//game-stats/game-counter/game-counter.component';
+import { GameHeaderComponent } from './game-header/game-header.component';
+import { GameStatsComponent } from './game-config/game-stats/game-stats.component';
+import { PatternsComponent } from './game-config/patterns/patterns.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameBoardComponent,
-    PatternsComponent,
+    GameButtonsComponent,
     GameConfigComponent,
     GameCounterComponent,
     GameHeaderComponent,
-    GameButtonsComponent,
-    GameGridComponent
+    GameStatsComponent,
+    PatternsComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ gameBoard: fromGameBoard.gameBoardReducer }),
+    StoreModule.forRoot({
+      gameBoard: fromGameBoard.gameBoardReducer,
+      gameConfig: fromGameConfig.gameConfigReducer,
+      gameStats: fromGameStats.gameStatsReducer,
+      patterns: fromPatterns.patternsReducer
+    }),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
     }),
     BrowserAnimationsModule,
     MaterialModule,
-    EffectsModule.forRoot([GameBoardEffects])
+    EffectsModule.forRoot([
+      GameBoardEffects,
+      GameConfigEffects,
+      GameStatsEffects,
+      PatternsEffects
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
