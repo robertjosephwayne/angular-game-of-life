639b33c3e59a834e41ed622f317269a5e7949c11
Robert <robertjwayne@gmail.com>
2020-12-03 14:57:20 +0700

    Move config actions from GameBoard to GameConfig

---
+++ src/app/game-config/store/game-config.reducer.ts
@@ -0,0 +1,118 @@
+import { Action, createReducer, on } from '@ngrx/store';
+
+import * as GameConfigActions from './game-config.actions';
+
+export interface State {
+  maxTickInterval: number;
+  autoTicking: boolean;
+  tickInterval: number;
+  ticker: any;
+  randomLifeActive: boolean;
+  minGridSize: number;
+  maxGridSize: number;
+  gridSize: number;
+}
+
+export const initialState: State = {
+  maxTickInterval: 1000,
+  autoTicking: false,
+  tickInterval: 500,
+  ticker: null,
+  randomLifeActive: false,
+  minGridSize: 10,
+  maxGridSize: 25,
+  gridSize: 10
+};
+
+const _gameConfigReducer = createReducer(
+  initialState,
+
+  on(GameConfigActions.startTicking, (state) => {
+    return {
+      ...state,
+      autoTicking: true
+    };
+  }),
+
+  on(GameConfigActions.stopTicking, (state) => {
+    return {
+      ...state,
+      autoTicking: false
+    };
+  }),
+
+  on(GameConfigActions.setTicker, (state, { newTicker }) => {
+    return {
+      ...state,
+      ticker: newTicker
+    };
+  }),
+
+  on(GameConfigActions.clearTicker, (state) => {
+    return {
+      ...state,
+      ticker: null
+    }
+  }),
+
+  on(GameConfigActions.setTickInterval, (state, { newTickInterval }) => {
+    return {
+      ...state,
+      tickInterval: newTickInterval,
+    };
+  }),
+
+  on(GameConfigActions.resetTickInterval, (state) => {
+    return {
+      ...state,
+      tickInterval: initialState.tickInterval
+    };
+  }),
+
+  on(GameConfigActions.emptyGenerationCheck, (state) => {
+    return {
+      ...state
+    };
+  }),
+
+  on(GameConfigActions.activateRandomLife, (state) => {
+    return {
+      ...state,
+      randomLifeActive: true
+    };
+  }),
+
+  on(GameConfigActions.disableRandomLife, (state) => {
+    return {
+      ...state,
+      randomLifeActive: false
+    };
+  }),
+
+  on(GameConfigActions.resetGridSize, (state) => {
+    return {
+      ...state,
+      gridSize: initialState.gridSize,
+    };
+  }),
+
+  on(GameConfigActions.zoomIn, (state) => {
+    const newGridSize = Math.max(state.minGridSize, state.gridSize - 1);
+    return {
+      ...state,
+      gridSize: newGridSize
+    };
+  }),
+
+  on(GameConfigActions.zoomOut, (state) => {
+    const newGridSize = Math.min(state.maxGridSize, state.gridSize + 1);
+    return {
+      ...state,
+      gridSize: newGridSize
+    };
+  }),
+);
+
+export function gameConfigReducer(state: State, action: Action) {
+  return _gameConfigReducer(state, action);
+}
