b33d74dfa3e251a6a661cf32487d1c2aec4f2b79
Robert <robertjwayne@gmail.com>
2020-12-03 14:44:01 +0700

    Remove clearTickInterval, setTickInterval, emptyGenerationCheck
    Create new effects: addRandomLiveCells and incrementGenerationCount

--- src/app/game-board/store/game-board.effects.ts
+++ src/app/game-board/store/game-board.effects.ts
@@ -4,41 +4,28 @@ import { Store } from '@ngrx/store';
 import { map, withLatestFrom } from 'rxjs/operators';
 
 import * as GameBoardActions from '../store/game-board.actions';
+import * as GameStatsActions from '../../game-config/game-stats/store/game-stats.actions';
+
 import * as fromApp from '../../store/app.reducer';
 
 @Injectable()
 export class GameBoardEffects {
 
-  clearTickInterval$ = createEffect(() => this.actions$.pipe(
-    ofType('[Game Board] Stop Ticking'),
-    withLatestFrom(this.store.select('gameBoard')),
-    map(([action, gameBoardState]) => {
-      clearInterval(gameBoardState.ticker);
-      return GameBoardActions.setTicker({ newTicker: null })
-    })
-  ));
-
-  setTickInterval$ = createEffect(() => this.actions$.pipe(
-    ofType('[Game Board] Start Ticking'),
-    withLatestFrom(this.store.select('gameBoard')),
-    map(([action, gameBoardState]) => {
-      clearInterval(gameBoardState.ticker);
-      const ticker = setInterval(() => {
-        this.store.dispatch(GameBoardActions.tick());
-        this.store.dispatch(GameBoardActions.emptyGenerationCheck());
-      }, gameBoardState.tickInterval);
-      return GameBoardActions.setTicker({ newTicker: ticker });
-    })
-  ));
-
-  emptyGenerationCheck$ = createEffect(() => this.actions$.pipe(
-    ofType('[Game Board] Empty Generation Check'),
-    withLatestFrom(this.store.select('gameBoard')),
-    map(([action, gameBoardState]) => {
-      if (!(gameBoardState.liveCells || gameBoardState.randomLifeActive)) {
-        return GameBoardActions.stopTicking();
+  addRandomLiveCells$ = createEffect(() => this.actions$.pipe(
+    ofType('[Game Board] Tick'),
+    withLatestFrom(this.store.select('gameConfig')),
+    map(([action, gameConfigState]) => {
+      if (gameConfigState.randomLifeActive) {
+        return GameBoardActions.addRandomLiveCell();
       }
       return { type: 'Empty Action' };
+    })
+  ));
+
+  incrementGenerationCount$ = createEffect(() => this.actions$.pipe(
+    ofType('[Game Board] Tick'),
+    map(() => {
+      return GameStatsActions.incrementGenerationCount();
     })
   ));
 
@@ -46,4 +33,5 @@ export class GameBoardEffects {
     private actions$: Actions,
     private store: Store<fromApp.AppState>
   ) { }
+
 }
