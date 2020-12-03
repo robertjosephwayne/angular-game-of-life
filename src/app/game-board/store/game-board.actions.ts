b64e4b6bd3301e84923f8470905ec9c15b831e39
Robert <robertjwayne@gmail.com>
2020-12-03 14:42:48 +0700

    Remove actions from GameBoard, move to separate components
    Remove resetGeneration 
    
    Move the following actions to Patterns:
    - setSelectedPattern
    
    Move the following actions to GameConfig:
    - setGridSize
    - resetGridSize
    - startTicking
    - stopTicking
    - setTicker
    - setTickInterval
    - resetTickInterval
    - activateRandomLife
    - disableRandomLife
    - zoomIn
    - zoomOut
    - emptyGenerationCheck

--- src/app/game-board/store/game-board.actions.ts
+++ src/app/game-board/store/game-board.actions.ts
@@ -1,55 +1,11 @@
 import { createAction, props } from '@ngrx/store';
 
-export const resetGeneration = createAction(
-  '[Game Board] Reset Generation'
-);
-
-export const setSelectedPattern = createAction(
-  '[Game Board] Set Selected Pattern',
-  props<{ patternName: string }>()
-);
-
-export const setGridSize = createAction(
-  '[Game Board] Set Grid Size',
-  props<{ gridSize: number }>()
-);
-
-export const resetGridSize = createAction(
-  '[Game Board] Reset Grid Size'
-);
-
 export const tick = createAction(
   '[Game Board] Tick'
 );
 
-export const startTicking = createAction(
-  '[Game Board] Start Ticking',
-);
-
-export const stopTicking = createAction(
-  '[Game Board] Stop Ticking'
-);
-
-export const setTicker = createAction(
-  '[Game Board] Set Ticker',
-  props<{ newTicker: any }>()
-);
-
-export const setTickInterval = createAction(
-  '[Game Board] Set Tick Interval',
-  props<{ newTickInterval: number }>()
-);
-
-export const resetTickInterval = createAction(
-  '[Game Board] Reset Tick Interval'
-);
-
-export const activateRandomLife = createAction(
-  '[Game Board] Activate Random Life'
-);
-
-export const disableRandomLife = createAction(
-  '[Game Board] Disable Random Life'
+export const addRandomLiveCell = createAction(
+  '[Game Board] Add Random Live Cell'
 );
 
 export const toggleCellLife = createAction(
@@ -57,14 +13,12 @@ export const toggleCellLife = createAction(
   props<{ rowIndex: number, columnIndex: number }>()
 );
 
-export const zoomIn = createAction(
-  '[Game Board] Zoom In'
+export const resizeCurrentGeneration = createAction(
+  '[Game Board] Resize Current Generation',
+  props<{ newSize: number }>()
 );
 
-export const zoomOut = createAction(
-  '[Game Board] Zoom Out'
-);
-
-export const emptyGenerationCheck = createAction(
-  '[Game Board] Empty Generation Check'
+export const setCurrentGeneration = createAction(
+  '[Game Board] Set Current Generation',
+  props<{ newGeneration: number[][] }>()
 );
