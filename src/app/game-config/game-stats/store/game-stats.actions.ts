66e4858a42800c93baef1a296538685f021a55e5
Robert <robertjwayne@gmail.com>
2020-12-03 14:53:05 +0700

    Add three new actions
    Add the following actions:
    
    - incrementGenerationCount
    - resetGenerationCount
    - setLiveCellCount

---
+++ src/app/game-config/game-stats/store/game-stats.actions.ts
@@ -0,0 +1,14 @@
+import { createAction, props } from '@ngrx/store';
+
+export const incrementGenerationCount = createAction(
+  '[Game Stats] Increment Generation Count'
+);
+
+export const resetGenerationCount = createAction(
+  '[Game Stats] Reset Generation Count'
+);
+
+export const setLiveCellCount = createAction(
+  '[Game Stats] Set Live Cell Count',
+  props<{ liveCells: number }>()
+)
