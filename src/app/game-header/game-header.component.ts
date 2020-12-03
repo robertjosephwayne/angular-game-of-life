e4e34673280ea133fbd981783709793e92d14095
Robert <robertjwayne@gmail.com>
2020-12-03 14:57:48 +0700

    Add label input to header

--- src/app/game-header/game-header.component.ts
+++ src/app/game-header/game-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-header',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-header.component.css']
 })
 export class GameHeaderComponent implements OnInit {
+  @Input() label: string;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
 }
