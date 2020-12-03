99fbeff655dbf5f75920b31e3d1a09e8f53c4610
Robert <robertjwayne@gmail.com>
2020-12-03 14:48:01 +0700

    Separate state and actions into new stores.
    Remove state properties, move them to individual stores for patterns, config, and game stats.
    
    Move actions into individual stores for patterns, config, and game states.
    
    Move pattern functions into Patterns component.

--- src/app/game-board/store/game-board.reducer.ts
+++ src/app/game-board/store/game-board.reducer.ts
@@ -4,18 +4,6 @@ import * as GameBoardActions from './game-board.actions';
 
 export interface State {
   currentGeneration: number[][];
-  generationCount: number;
-  presetPatterns: string[];
-  selectedPattern: string;
-  minGridSize: number;
-  maxGridSize: number;
-  gridSize: number;
-  autoTicking: boolean;
-  tickInterval: number;
-  maxTickInterval: number;
-  ticker: any;
-  randomLifeActive: boolean;
-  liveCells: number;
 }
 
 export const initialState: State = {
@@ -30,182 +18,49 @@ export const initialState: State = {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ],
-  generationCount: 0,
-  presetPatterns: [
-    'Empty',
-    'Glider',
-    'Small Exploder',
-    'Exploder',
-    'Ten Cell Row',
-    'Lightweight Spaceship',
-    'Block',
-    'Tub',
-    'Boat'
-  ],
-  selectedPattern: 'Empty',
-  minGridSize: 10,
-  maxGridSize: 25,
-  gridSize: 10,
-  autoTicking: false,
-  tickInterval: 500,
-  maxTickInterval: 1000,
-  ticker: null,
-  randomLifeActive: false,
-  liveCells: 0
+  ]
 };
 
 const _gameBoardReducer = createReducer(
   initialState,
 
-  on(GameBoardActions.resetGeneration, (state) => {
-    const initialGeneration = getSelectedPattern(state.selectedPattern, state.gridSize);
-    const liveCells = countLiveCells(initialGeneration);
-    return {
-      ...state,
-      currentGeneration: initialGeneration,
-      generationCount: 0,
-      liveCells
-    };
-  }),
-
-  on(GameBoardActions.setSelectedPattern, (state, { patternName }) => {
-    const newGeneration = getSelectedPattern(patternName, state.gridSize);
-    const liveCells = countLiveCells(newGeneration);
-    return {
-      ...state,
-      selectedPattern: patternName,
-      currentGeneration: newGeneration,
-      liveCells,
-      generationCount: 0
-    };
-  }),
-
-  on(GameBoardActions.setGridSize, (state, { gridSize }) => {
-    let newGridSize = gridSize;
-    if (newGridSize >= state.maxGridSize) {
-      newGridSize = state.maxGridSize;
-    } else if (newGridSize <= state.minGridSize) {
-      newGridSize = state.minGridSize;
-    }
-    const currentGenerationResized = resizeCurrentGeneration(state.currentGeneration, newGridSize);
-    const liveCells = countLiveCells(currentGenerationResized);
-    return {
-      ...state,
-      gridSize: newGridSize,
-      currentGeneration: currentGenerationResized,
-      liveCells
-    };
-  }),
-
-  on(GameBoardActions.resetGridSize, (state) => {
-    return {
-      ...state,
-      gridSize: initialState.gridSize,
-    };
-  }),
-
   on(GameBoardActions.tick, (state) => {
-    let updatedGeneration = getNextGeneration(state.currentGeneration, state.gridSize);
-    if (state.randomLifeActive) {
-      updatedGeneration = addRandomLife(updatedGeneration, state.gridSize);
+    const nextGeneration = getNextGeneration(state.currentGeneration);
+    return {
+      ...state,
+      currentGeneration: nextGeneration
+    };
+  }),
+
+  on(GameBoardActions.addRandomLiveCell, (state) => {
+    let updatedGeneration = addRandomLife(state.currentGeneration);
+    return {
+      ...state,
+      currentGeneration: updatedGeneration
     }
-    const liveCells = countLiveCells(updatedGeneration);
-    return {
-      ...state,
-      currentGeneration: updatedGeneration,
-      generationCount: state.generationCount + 1,
-      liveCells
-    };
-  }),
-
-  on(GameBoardActions.startTicking, (state) => {
-    return {
-      ...state,
-      autoTicking: true
-    };
-  }),
-
-  on(GameBoardActions.stopTicking, (state) => {
-    return {
-      ...state,
-      autoTicking: false
-    };
-  }),
-
-  on(GameBoardActions.setTicker, (state, { newTicker }) => {
-    return {
-      ...state,
-      ticker: newTicker
-    };
-  }),
-
-  on(GameBoardActions.setTickInterval, (state, { newTickInterval }) => {
-    return {
-      ...state,
-      tickInterval: newTickInterval,
-    };
-  }),
-
-  on(GameBoardActions.resetTickInterval, (state) => {
-    return {
-      ...state,
-      tickInterval: initialState.tickInterval
-    };
-  }),
-
-  on(GameBoardActions.activateRandomLife, (state) => {
-    return {
-      ...state,
-      randomLifeActive: true
-    };
-  }),
-
-  on(GameBoardActions.disableRandomLife, (state) => {
-    return {
-      ...state,
-      randomLifeActive: false
-    };
   }),
 
   on(GameBoardActions.toggleCellLife, (state, { rowIndex, columnIndex }) => {
     const updatedGeneration = toggleCellLife(state.currentGeneration, rowIndex, columnIndex);
-    const liveCells = countLiveCells(updatedGeneration);
     return {
       ...state,
-      currentGeneration: updatedGeneration,
-      liveCells
+      currentGeneration: updatedGeneration
     };
   }),
 
-  on(GameBoardActions.zoomIn, (state) => {
-    const gridSize = Math.max(state.minGridSize, state.gridSize - 1);
-    const currentGenerationResized = resizeCurrentGeneration(state.currentGeneration, gridSize);
-    const liveCells = countLiveCells(currentGenerationResized);
+  on(GameBoardActions.resizeCurrentGeneration, (state, { newSize }) => {
+    const currentGenerationResized = resizeCurrentGeneration(state.currentGeneration, newSize);
     return {
       ...state,
-      currentGeneration: currentGenerationResized,
-      gridSize,
-      liveCells
-    };
+      currentGeneration: currentGenerationResized
+    }
   }),
 
-  on(GameBoardActions.zoomOut, (state) => {
-    const gridSize = Math.min(state.maxGridSize, state.gridSize + 1);
-    const currentGenerationResized = resizeCurrentGeneration(state.currentGeneration, gridSize);
-    const liveCells = countLiveCells(currentGenerationResized);
+  on(GameBoardActions.setCurrentGeneration, (state, { newGeneration }) => {
     return {
       ...state,
-      currentGeneration: currentGenerationResized,
-      gridSize,
-      liveCells
-    };
-  }),
-
-  on(GameBoardActions.emptyGenerationCheck, (state) => {
-    return {
-      ...state
-    };
+      currentGeneration: newGeneration
+    }
   })
 );
 
@@ -213,7 +68,8 @@ export function gameBoardReducer(state: State, action: Action) {
   return _gameBoardReducer(state, action);
 }
 
-function getNextGeneration(currentGeneration: number[][], gridSize: number) {
+function getNextGeneration(currentGeneration: number[][]) {
+  const gridSize = currentGeneration.length;
   let nextGeneration = [];
 
   for (let i = 0; i < gridSize; i++) {
@@ -285,13 +141,13 @@ function toggleCellLife(generation: number[][], rowIndex: number, columnIndex: n
   return updatedGeneration;
 }
 
-function resizeCurrentGeneration(currentGeneration: number[][], gridSize: number) {
-  const currentGenerationSize = currentGeneration.length;
+function resizeCurrentGeneration(currentGeneration: number[][], newSize: number) {
+  const currentSize = currentGeneration.length;
   const currentGenerationResized = [];
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < newSize; i++) {
     currentGenerationResized[i] = [];
-    for (let j = 0; j < gridSize; j++) {
-      if (i >= currentGenerationSize || j >= currentGenerationSize) {
+    for (let j = 0; j < newSize; j++) {
+      if (i >= currentSize || j >= currentSize) {
         currentGenerationResized[i][j] = 0;
       } else {
         currentGenerationResized[i][j] = currentGeneration[i][j];
@@ -301,183 +157,20 @@ function resizeCurrentGeneration(currentGeneration: number[][], gridSize: number
   return currentGenerationResized;
 }
 
-function countLiveCells(generation: number[][]) {
-  const rows = generation.length;
-  const columns = generation[0].length;
-  let liveCells = 0;
-
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-      liveCells += generation[i][j];
-    }
-  }
-
-  return liveCells;
-}
-
-function addRandomLife(generation: number[][], gridSize: number) {
+function addRandomLife(generation: number[][]) {
+  let updatedGeneration = [];
+  generation.forEach(row => {
+    updatedGeneration.push(row.slice());
+  });
+  const gridSize = generation.length;
   const randomRow = Math.floor(Math.random() * gridSize);
   const randomColumn = Math.floor(Math.random() * gridSize);
   const currentCellValue = generation[randomRow][randomColumn];
   if (currentCellValue) {
-    return addRandomLife(generation, gridSize);
+    return addRandomLife(generation);
   } else {
-    generation[randomRow][randomColumn] = 1;
-    return generation;
+    updatedGeneration[randomRow][randomColumn] = 1;
+    return updatedGeneration;
   }
 }
 
-function getSelectedPattern(selectedPattern: string, gridSize: number) {
-  let pattern = getEmptyGeneration(gridSize);
-
-  switch (selectedPattern) {
-    case 'Glider':
-      pattern = getGliderPattern(gridSize);
-      break;
-    case 'Small Exploder':
-      pattern = getSmallExploderPattern(gridSize);
-      break;
-    case 'Exploder':
-      pattern = getExploderPattern(gridSize);
-      break;
-    case 'Ten Cell Row':
-      pattern = getTenCellRowPattern(gridSize);
-      break;
-    case 'Lightweight Spaceship':
-      pattern = getLightweightSpaceshipPattern(gridSize);
-      break;
-    case 'Block':
-      pattern = getBlockPattern(gridSize);
-      break;
-    case 'Tub':
-      pattern = getTubPattern(gridSize);
-      break;
-    case 'Boat':
-      pattern = getBoatPattern(gridSize);
-      break;
-    default:
-      break;
-  }
-
-  return pattern;
-}
-
-function getEmptyGeneration(gridSize: number) {
-  const emptyGeneration = [];
-
-  for (let i = 0; i < gridSize; i++) {
-    emptyGeneration[i] = [];
-    for (let j = 0; j < gridSize; j++) {
-      emptyGeneration[i][j] = 0;
-    }
-  }
-
-  return emptyGeneration;
-}
-
-function getGliderPattern(gridSize: number) {
-  const gliderPattern = getEmptyGeneration(gridSize);
-  const startingRow = Math.floor((gridSize - 1) / 2 - 1);
-  const startingColumn = Math.floor((gridSize - 1) / 2 - 1);
-  gliderPattern[startingRow][startingColumn + 1] = 1;
-  gliderPattern[startingRow + 1][startingColumn + 2] = 1;
-  gliderPattern[startingRow + 2][startingColumn] = 1;
-  gliderPattern[startingRow + 2][startingColumn + 1] = 1;
-  gliderPattern[startingRow + 2][startingColumn + 2] = 1;
-  return gliderPattern;
-}
-
-function getSmallExploderPattern(gridSize: number) {
-  const smallExploderPattern = getEmptyGeneration(gridSize);
-  const startingRow = Math.floor((gridSize - 1) / 2 - 1);
-  const startingColumn = Math.floor((gridSize - 1) / 2 - 1);
-  smallExploderPattern[startingRow][startingColumn + 1] = 1;
-  smallExploderPattern[startingRow + 1][startingColumn] = 1;
-  smallExploderPattern[startingRow + 1][startingColumn + 1] = 1;
-  smallExploderPattern[startingRow + 1][startingColumn + 2] = 1;
-  smallExploderPattern[startingRow + 2][startingColumn] = 1;
-  smallExploderPattern[startingRow + 2][startingColumn + 2] = 1;
-  smallExploderPattern[startingRow + 3][startingColumn + 1] = 1;
-  return smallExploderPattern;
-}
-
-function getExploderPattern(gridSize: number) {
-  const exploderPattern = getEmptyGeneration(gridSize);
-  const startingRow = Math.floor((gridSize - 1) / 2 - 2);
-  const startingColumn = Math.floor((gridSize - 1) / 2 - 2);
-  exploderPattern[startingRow][startingColumn] = 1;
-  exploderPattern[startingRow][startingColumn + 2] = 1;
-  exploderPattern[startingRow][startingColumn + 4] = 1;
-  exploderPattern[startingRow + 1][startingColumn] = 1;
-  exploderPattern[startingRow + 1][startingColumn + 4] = 1;
-  exploderPattern[startingRow + 2][startingColumn] = 1;
-  exploderPattern[startingRow + 2][startingColumn + 4] = 1;
-  exploderPattern[startingRow + 3][startingColumn] = 1;
-  exploderPattern[startingRow + 3][startingColumn + 4] = 1;
-  exploderPattern[startingRow + 4][startingColumn] = 1;
-  exploderPattern[startingRow + 4][startingColumn + 2] = 1;
-  exploderPattern[startingRow + 4][startingColumn + 4] = 1;
-  return exploderPattern;
-}
-
-function getTenCellRowPattern(gridSize: number) {
-  const startingRow = Math.floor((gridSize - 1) / 2);
-  const startingColumn = Math.floor((gridSize - 1) / 2 - 4);
-  const tenCellRowPattern = getEmptyGeneration(gridSize);
-  const endingColumn = startingColumn + 10;
-  for (let i = startingColumn; i < endingColumn; i++) {
-    tenCellRowPattern[startingRow][i] = 1;
-  }
-
-  return tenCellRowPattern;
-}
-
-function getLightweightSpaceshipPattern(gridSize: number) {
-  const lightweightSpaceshipPattern = getEmptyGeneration(gridSize);
-  const startingRow = Math.floor((gridSize - 1) / 2 - 1);
-  const startingColumn = Math.floor((gridSize - 1) / 2 - 2);
-  lightweightSpaceshipPattern[startingRow][startingColumn + 1] = 1;
-  lightweightSpaceshipPattern[startingRow][startingColumn + 2] = 1;
-  lightweightSpaceshipPattern[startingRow][startingColumn + 3] = 1;
-  lightweightSpaceshipPattern[startingRow][startingColumn + 4] = 1;
-  lightweightSpaceshipPattern[startingRow + 1][startingColumn] = 1;
-  lightweightSpaceshipPattern[startingRow + 1][startingColumn + 4] = 1;
-  lightweightSpaceshipPattern[startingRow + 2][startingColumn + 4] = 1;
-  lightweightSpaceshipPattern[startingRow + 3][startingColumn] = 1;
-  lightweightSpaceshipPattern[startingRow + 3][startingColumn + 3] = 1;
-  return lightweightSpaceshipPattern;
-}
-
-function getBlockPattern(gridSize: number) {
-  const blockPattern = getEmptyGeneration(gridSize);
-  const startingRow = Math.floor((gridSize - 1) / 2);
-  const startingColumn = Math.floor((gridSize - 1) / 2);
-  blockPattern[startingRow][startingColumn] = 1;
-  blockPattern[startingRow][startingColumn + 1] = 1;
-  blockPattern[startingRow + 1][startingColumn] = 1;
-  blockPattern[startingRow + 1][startingColumn + 1] = 1;
-  return blockPattern;
-}
-
-function getTubPattern(gridSize: number) {
-  const tubPattern = getEmptyGeneration(gridSize);
-  const startingRow = Math.floor((gridSize - 1) / 2 - 1);
-  const startingColumn = Math.floor((gridSize - 1) / 2 - 1);
-  tubPattern[startingRow][startingColumn + 1] = 1;
-  tubPattern[startingRow + 1][startingColumn] = 1;
-  tubPattern[startingRow + 1][startingColumn + 2] = 1;
-  tubPattern[startingRow + 2][startingColumn + 1] = 1;
-  return tubPattern;
-}
-
-function getBoatPattern(gridSize: number) {
-  const boatPattern = getEmptyGeneration(gridSize);
-  const startingRow = Math.floor((gridSize - 1) / 2 - 1);
-  const startingColumn = Math.floor((gridSize - 1) / 2 - 1);
-  boatPattern[startingRow][startingColumn + 1] = 1;
-  boatPattern[startingRow + 1][startingColumn] = 1;
-  boatPattern[startingRow + 1][startingColumn + 2] = 1;
-  boatPattern[startingRow + 2][startingColumn + 1] = 1;
-  boatPattern[startingRow + 2][startingColumn + 2] = 1;
-  return boatPattern;
-}
