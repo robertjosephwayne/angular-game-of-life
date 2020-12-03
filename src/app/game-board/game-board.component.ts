843c2d6b60128a1e90812155f5acb5d08579c63b
Robert <robertjwayne@gmail.com>
2020-12-03 14:38:26 +0700

    Move GameGrid functionality back into GameBoard

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+
+import * as GameBoardActions from '../game-board/store/game-board.actions';
+
+import * as fromApp from '../store/app.reducer';
 
 @Component({
   selector: 'app-game-board',
@@ -6,9 +12,28 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./game-board.component.css']
 })
 export class GameBoardComponent implements OnInit, OnDestroy {
-  constructor() { }
+  gameBoardSub: Subscription;
+  currentGeneration: number[][];
 
-  ngOnInit(): void { }
+  constructor(
+    private store: Store<fromApp.AppState>
+  ) { }
 
-  ngOnDestroy(): void { }
+  ngOnInit(): void {
+    this.setGameBoardData();
+  }
+
+  setGameBoardData(): void {
+    this.gameBoardSub = this.store.select('gameBoard').subscribe(state => {
+      this.currentGeneration = state.currentGeneration;
+    });
+  }
+
+  handleCellClick(rowIndex: number, columnIndex: number): void {
+    this.store.dispatch(GameBoardActions.toggleCellLife({ rowIndex, columnIndex }));
+  }
+
+  ngOnDestroy(): void {
+    this.gameBoardSub.unsubscribe();
+  }
 }
