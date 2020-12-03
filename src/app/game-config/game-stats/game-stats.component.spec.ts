8770c62f251a767a1c1564217ccf47432ec9d867
Robert <robertjwayne@gmail.com>
2020-12-03 14:51:53 +0700

    Initial commit

--- src/app/game-config/game-stats/game-stats.component.spec.ts
+++ src/app/game-config/game-stats/game-stats.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GameGridComponent } from './game-grid.component';
+import { GameStatsComponent } from './game-stats.component';
 
-describe('GameGridComponent', () => {
-  let component: GameGridComponent;
-  let fixture: ComponentFixture<GameGridComponent>;
+describe('GameStatsComponent', () => {
+  let component: GameStatsComponent;
+  let fixture: ComponentFixture<GameStatsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameGridComponent ]
+      declarations: [ GameStatsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GameGridComponent);
+    fixture = TestBed.createComponent(GameStatsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
