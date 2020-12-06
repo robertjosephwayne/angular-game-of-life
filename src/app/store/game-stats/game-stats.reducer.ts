64b768095cfbf5c65c61ed678753788e99358964
Robert <robertjwayne@gmail.com>
2020-12-06 12:39:49 +0700

    Remove generationCount from reducer

--- src/app/store/game-stats/game-stats.reducer.ts
+++ src/app/store/game-stats/game-stats.reducer.ts
@@ -3,31 +3,15 @@ import { Action, createReducer, on } from '@ngrx/store';
 import * as GameStatsActions from './game-stats.actions';
 
 export interface State {
-  generationCount: number;
   liveCells: number;
 }
 
 export const initialState: State = {
-  generationCount: 0,
   liveCells: 0
 };
 
 const _gameStatsReducer = createReducer(
   initialState,
-
-  on(GameStatsActions.incrementGenerationCount, (state) => {
-    return {
-      ...state,
-      generationCount: state.generationCount + 1
-    };
-  }),
-
-  on(GameStatsActions.resetGenerationCount, (state) => {
-    return {
-      ...state,
-      generationCount: 0
-    };
-  }),
 
   on(GameStatsActions.updateLiveCellCount, (state) => {
     return {
