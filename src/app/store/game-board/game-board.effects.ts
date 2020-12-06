d610a5d95a586930b982fe5ee850583ac6aac374
Robert <robertjwayne@gmail.com>
2020-12-06 14:58:46 +0700

    Add autoTick effect

--- src/app/store/game-board/game-board.effects.ts
+++ src/app/store/game-board/game-board.effects.ts
@@ -4,11 +4,24 @@ import { Store } from '@ngrx/store';
 import { map, withLatestFrom } from 'rxjs/operators';
 
 import * as GameBoardActions from './game-board.actions';
+import * as TickerActions from '../ticker/ticker.actions';
 
 import * as fromApp from '../app.reducer';
 
 @Injectable()
 export class GameBoardEffects {
+
+  autoTick$ = createEffect(() => this.actions$.pipe(
+    ofType('[Ticker Effect] Auto Tick'),
+    withLatestFrom(this.store.select('gameBoard')),
+    map(([action, gameBoardState]) => {
+      if (!(gameBoardState.liveCells || gameBoardState.randomLifeActive)) {
+        return TickerActions.stopTicking();
+      } else {
+        return GameBoardActions.tick();
+      }
+    })
+  ));
 
   constructor(
     private actions$: Actions,
