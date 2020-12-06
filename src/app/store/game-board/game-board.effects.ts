5f53aa235da45c9b977d68c94a4f682fe9e7ceed
Robert <robertjwayne@gmail.com>
2020-12-06 13:17:16 +0700

    Remove updateLiveCellCount effect

--- src/app/store/game-board/game-board.effects.ts
+++ src/app/store/game-board/game-board.effects.ts
@@ -4,7 +4,6 @@ import { Store } from '@ngrx/store';
 import { map, withLatestFrom } from 'rxjs/operators';
 
 import * as GameBoardActions from './game-board.actions';
-import * as GameStatsActions from '../game-stats/game-stats.actions';
 
 import * as fromApp from '../app.reducer';
 
@@ -21,20 +20,6 @@ export class GameBoardEffects {
       return { type: 'Empty Action' };
     })
   ));
-
-  updateLiveCellCount$ = createEffect(() => this.actions$.pipe(
-    ofType(
-      '[Game Board] Reset Generation',
-      '[Game Board] Tick',
-      '[Game Board] Add Random Live Cell',
-      '[Game Board] Toggle Cell Life',
-      '[Game Board] Set Current Generation',
-      '[Game Board] Resize Current Generation'
-    ),
-    map(() => {
-      return GameStatsActions.updateLiveCellCount();
-    })
-  ))
 
   constructor(
     private actions$: Actions,
