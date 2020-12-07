aa609261520562db643deed862ec577ba65c45e8
Robert <robertjwayne@gmail.com>
2020-12-07 14:46:03 +0700

    Remove unused imports

--- src/app/store/game-board/game-board.effects.ts
+++ src/app/store/game-board/game-board.effects.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, withLatestFrom } from 'rxjs/operators';
-
-import * as GameBoardActions from './game-board.actions';
-import * as TickerActions from '../ticker/ticker.actions';
 
 import * as fromApp from '../app.reducer';
 
