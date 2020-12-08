9850c66c2d7525a728ee2d2063c0080905b6aad4
Robert <robertjwayne@gmail.com>
2020-12-08 19:55:46 +0700

    currentGeneration is now an observable

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import * as GameBoardActions from '../../store/game-board/game-board.actions';
 
 import * as fromApp from '../../store/app.reducer';
+import * as fromGameBoard from '../../store/game-board/game-board.selectors';
 
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.css']
 })
-export class GameBoardComponent implements OnInit, OnDestroy {
-  gameBoardSub: Subscription;
-  currentGeneration: number[][];
+export class GameBoardComponent implements OnInit {
+  currentGeneration$: Observable<number[][]>;
 
   constructor(
     private store: Store<fromApp.AppState>
