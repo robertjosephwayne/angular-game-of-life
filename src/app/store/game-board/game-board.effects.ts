31b219d5eaf958ee1b1a569c284123c416edfed3
Robert <robertjwayne@gmail.com>
2020-12-08 16:08:02 +0700

    Trigger nextGeneration effect on each tick action from Ticker

--- src/app/store/game-board/game-board.effects.ts
+++ src/app/store/game-board/game-board.effects.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 
 import * as fromApp from '../app.reducer';
 
+import * as GameBoardActions from './game-board.actions';
+
 @Injectable()
 export class GameBoardEffects {
+
+  nextGeneration$ = createEffect(() => this.actions$.pipe(
+    ofType('[Ticker Effect] Tick'),
+    map(() => {
+      return GameBoardActions.nextGeneration();
+    })
+  ));
 
   constructor(
     private actions$: Actions,
