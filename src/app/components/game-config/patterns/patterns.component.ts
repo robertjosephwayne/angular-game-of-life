dc8f762d354d30215cdc8bde10fef9becf93c28d
Robert <robertjwayne@gmail.com>
2020-12-08 20:00:24 +0700

    Get presetPatterns and selectedPattern using selectors.

--- src/app/components/game-config/patterns/patterns.component.ts
+++ src/app/components/game-config/patterns/patterns.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
-import * as TickerActions from '../../../store/ticker/ticker.actions';
 import * as PatternsActions from '../../../store/patterns/patterns.actions';
 
 import * as fromApp from '../../../store/app.reducer';
+import * as fromPatterns from '../../../store/patterns/patterns.selectors';
 
 @Component({
   selector: 'app-patterns',
   templateUrl: './patterns.component.html',
   styleUrls: ['./patterns.component.css']
 })
-export class PatternsComponent implements OnInit, OnDestroy {
-  patternsSub: Subscription;
-  presetPatterns: string[];
-  selectedPattern: string;
+export class PatternsComponent implements OnInit {
+  presetPatterns$: Observable<string[]>;
+  selectedPattern$: Observable<string>;
 
   constructor(
     private store: Store<fromApp.AppState>
@@ -26,17 +25,11 @@ export class PatternsComponent implements OnInit, OnDestroy {
   }
 
   setPatternsData(): void {
-    this.patternsSub = this.store.select('patterns').subscribe(state => {
-      this.selectedPattern = state.selectedPattern;
-      this.presetPatterns = state.presetPatterns;
-    });
+    this.presetPatterns$ = this.store.select(fromPatterns.selectPresetPatterns);
+    this.selectedPattern$ = this.store.select(fromPatterns.selectedPattern);
   }
 
   handlePatternSelect(patternName: string): void {
     this.store.dispatch(PatternsActions.setSelectedPattern({ patternName }));
-  }
-
-  ngOnDestroy(): void {
-    this.patternsSub.unsubscribe();
   }
 }
