6650d4e509963251f815246859a129518d293236
Robert <robertjwayne@gmail.com>
2020-12-09 11:54:40 +0700

    Move grid logic to GameGrid component

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -1,33 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-
-import * as GameBoardActions from '../../store/game-board/game-board.actions';
-
-import * as fromApp from '../../store/app.reducer';
-import * as fromGameBoard from '../../store/game-board/game-board.selectors';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.css']
 })
-export class GameBoardComponent implements OnInit {
-  currentGeneration$: Observable<number[][]>;
-
-  constructor(
-    private store: Store<fromApp.AppState>
-  ) { }
-
-  ngOnInit(): void {
-    this.setGameBoardData();
-  }
-
-  setGameBoardData(): void {
-    this.currentGeneration$ = this.store.select(fromGameBoard.selectCurrentGeneration);
-  }
-
-  handleCellClick(rowIndex: number, columnIndex: number): void {
-    this.store.dispatch(GameBoardActions.toggleCellLife({ rowIndex, columnIndex }));
-  }
-}
+export class GameBoardComponent { }
