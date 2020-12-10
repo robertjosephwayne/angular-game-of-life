e0e8345b6fc01456aaccdc54097c9dd5eddd53c7
Robert <robertjwayne@gmail.com>
2020-12-10 16:55:22 +0700

    Create test for Ticker selector: selectTicker
    selectTicker should return the ticker state

--- src/app/store/ticker/ticker.selectors.spec.ts
+++ src/app/store/ticker/ticker.selectors.spec.ts
@@ -4,5 +4,11 @@ import * as fromTicker from './ticker.selectors';
 import { initialState, TickerState } from './ticker.state';
 
 describe('Ticker Selectors', () => {
-  
+
+  describe('selectTicker', () => {
+    it('should return the ticker state', () => {
+      const appState = mockState();
+      expect(fromTicker.selectTicker(appState)).toEqual(initialState);
+    });
+  });
 })
