2dfcce6255e47efcfc688dd8b1b8a9bea695ff93
Robert <robertjwayne@gmail.com>
2020-12-10 18:02:34 +0700

    Set up testing file for Game Grid Component

--- src/app/components/game-board/game-grid/game-grid.component.spec.ts
+++ src/app/components/game-board/game-grid/game-grid.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+
+import { AppState, mockState } from 'src/app/store/app.state';
 
 import { GameGridComponent } from './game-grid.component';
 
 describe('GameGridComponent', () => {
   let component: GameGridComponent;
   let fixture: ComponentFixture<GameGridComponent>;
+  let store: MockStore<AppState>;
+  let dispatchSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameGridComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [
+        GameGridComponent
+      ],
+      providers: [
+        provideMockStore<AppState>({
+          initialState: mockState()
+        })
+      ]
+    }).compileComponents();
 
-  beforeEach(() => {
+    store = TestBed.inject(MockStore);
+    dispatchSpy = spyOn(store, "dispatch");
     fixture = TestBed.createComponent(GameGridComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
