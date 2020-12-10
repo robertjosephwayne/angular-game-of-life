4054641f6ef7ba4e382ff1432bd1fea42e69c5b4
Robert <robertjwayne@gmail.com>
2020-12-10 16:53:57 +0700

    Import TickerState

--- src/app/store/ticker/ticker.selectors.spec.ts
+++ src/app/store/ticker/ticker.selectors.spec.ts
@@ -1,7 +1,7 @@
 import { mockState } from '../app.state';
 
 import * as fromTicker from './ticker.selectors';
-import { initialState } from './ticker.state';
+import { initialState, TickerState } from './ticker.state';
 
 describe('Ticker Selectors', () => {
   
