f12eae64c4f8874e5480450f418669d871807a9b
Robert <robertjwayne@gmail.com>
2020-12-10 10:13:03 +0700

    Set up initial action tests
    - Add test for init action
    - Add test for unknown action
    - Add shell for each Game Board action

---
+++ src/app/store/patterns/patterns.reducer.spec.ts
@@ -0,0 +1,34 @@
+import { INIT } from '@ngrx/store';
+
+import { patternsReducer } from './patterns.reducer';
+import { initialState } from './patterns.state';
+
+describe('Patterns Reducer', () => {
+
+  describe('Patterns Actions', () => {
+
+    describe('init action', () => {
+      it('should return the initial state', () => {
+        const nextState = patternsReducer(undefined, { type: INIT });
+        expect(nextState).toBe(initialState);
+      });
+    });
+
+    describe('unknown action', () => {
+      it('should return the previous state', () => {
+        const nextState = patternsReducer(initialState, {} as any);
+        expect(nextState).toBe(initialState);
+      });
+    });
+
+    describe('setSelectedPattern action', () => {
+
+    });
+
+    describe('resetSelectedPattern action', () => {
+
+    });
+
+  });
+
+});
