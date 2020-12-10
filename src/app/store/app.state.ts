4adbd7f294509f9b836f6d618bc9f0487c7b2f97
Robert <robertjwayne@gmail.com>
2020-12-10 16:30:00 +0700

    Add mockState

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,3 +1,7 @@
+import { INIT } from '@ngrx/store';
+
+import { appReducer } from './app.reducer';
+
 import { GameBoardState } from './game-board/game-board.state';
 import { TickerState } from './ticker/ticker.state';
 import { PatternsState } from './patterns/patterns.state';
@@ -7,3 +11,16 @@ export interface AppState {
   ticker: TickerState;
   patterns: PatternsState;
 }
+
+export const mockState = (override: Partial<AppState> = {}): AppState => {
+  const initialState = {};
+
+  Object.entries(appReducer).forEach(([key, reducer]) => {
+    initialState[key] = reducer(undefined, { type: INIT });
+  });
+
+  return {
+    ...initialState,
+    ...override
+  } as AppState;
+};
