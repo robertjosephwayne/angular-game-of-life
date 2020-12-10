dd2c42ce6dba8cb0659665a38777ecfe616be8c8
Robert <robertjwayne@gmail.com>
2020-12-10 10:12:48 +0700

    Set up initial action tests
    - Add test for init action
    - Add test for unknown action
    - Add test for reset action
    - Add shell for each Game Board action

---
+++ src/app/store/game-board/game-board.reducer.spec.ts
@@ -0,0 +1,61 @@
+import { INIT } from '@ngrx/store';
+
+import * as GameBoardActions from './game-board.actions';
+import { gameBoardReducer } from './game-board.reducer';
+import { initialState } from './game-board.state';
+
+describe('Game Board Reducer', () => {
+
+  describe('Game Board Actions', () => {
+
+    describe('init action', () => {
+      it('should return the initial state', () => {
+        const nextState = gameBoardReducer(undefined, { type: INIT });
+        expect(nextState).toBe(initialState);
+      });
+    });
+
+    describe('unknown action', () => {
+      it('should return the previous state', () => {
+        const nextState = gameBoardReducer(initialState, {} as any);
+        expect(nextState).toBe(initialState);
+      });
+    });
+
+    describe('toggleCellLife action', () => {
+
+    });
+
+    describe('zoomIn action', () => {
+
+    });
+
+    describe('zoomOut action', () => {
+
+    });
+
+    describe('activateRandomLife action', () => {
+
+    });
+
+    describe('disableRandomLife action', () => {
+
+    });
+
+    describe('setCurrentGeneration action', () => {
+
+    });
+
+    describe('nextGeneration action', () => {
+
+    });
+
+    describe('reset action', () => {
+      it('should return the previous state', () => {
+        const nextState = gameBoardReducer(initialState, GameBoardActions.reset());
+        expect(nextState).toBe(initialState);
+      });
+    });
+
+  });
+});
