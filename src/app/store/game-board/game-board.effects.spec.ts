e2d41d3bd620c56ded031cfa12d82165e77a7fa9
Robert <robertjwayne@gmail.com>
2020-12-11 18:19:37 +0700

    Create test for Game Board Effects: nextGeneration$
    nextGeneration$ should dispatch the nextGeneration action

---
+++ src/app/store/game-board/game-board.effects.spec.ts
@@ -0,0 +1,37 @@
+import { Action } from '@ngrx/store';
+import { TestBed } from "@angular/core/testing";
+import { provideMockActions } from "@ngrx/effects/testing";
+
+import { Observable, of } from 'rxjs';
+
+import * as GameBoardActions from '../game-board/game-board.actions';
+import * as TickerActions from '../ticker/ticker.actions';
+
+import { GameBoardEffects } from './game-board.effects';
+import { toArray } from 'rxjs/operators';
+
+describe('Game Board Effects', () => {
+  let actions$: Observable<Action>;
+  let effects: GameBoardEffects;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        GameBoardEffects,
+        provideMockActions(() => actions$)
+      ]
+    });
+    effects = TestBed.inject(GameBoardEffects);
+  });
+
+  describe('nextGeneration$', () => {
+    it('should dispatch the nextGeneration action', () => {
+      actions$ = of(TickerActions.tick());
+      effects.nextGeneration$.subscribe((actions) => {
+        expect(actions).toEqual(
+          GameBoardActions.nextGeneration()
+        );
+      });
+    });
+  });
+});
